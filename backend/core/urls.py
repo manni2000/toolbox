8944bc014070feb1c96de623180d2a15fe1c5f9f
Manish kumar <manishmandal9734@gmail.com>
2026-01-27 16:07:49 +0530

    update

--- backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-from django.urls import path
-
-def home(request):
-    return JsonResponse({"status": "Django running on Vercel 🚀"})
-
-urlpatterns = [
-    path("", home),
-]
