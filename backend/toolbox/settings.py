d27630b2ec706c58df62554d5fdd6854f636a986
Manish kumar <manishmandal9734@gmail.com>
2026-01-28 18:11:57 +0530

    update

--- backend/toolbox/settings.py
+++ backend/toolbox/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-chang
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
